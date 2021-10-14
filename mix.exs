107b00e1d026463f788a262e81e07d812404d00e
Kristoffer Søholm <k.soeholm@gmail.com>
2021-10-14 16:27:58 +0200

    Bump rustler to 0.22.2

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule MixBlake3.Project do
 
   defp deps do
     [
-      {:rustler, "~> 0.21.1"},
+      {:rustler, "~> 0.22.2"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
